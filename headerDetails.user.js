a3632c6621f21e779d5a3fd401c0ef1728f7b673
ibettison <ian.bettison@ncl.ac.uk>
2017-04-18 16:08:22 +0100

    The changes made this time were to remove unnecessary detail

--- headerDetails.user.js
+++ headerDetails.user.js
@@ -15,25 +15,6 @@
     var signOff                 = "\n\nThanks for your attention,\n\n"+raisedUserFull+"\n"+FMSLocation+"\n"+FMSTelephone+"\n"+FMSEmail+"\n";
 
     /*setup the default responses to your canned answers here*/
-    var cannedAnswersSummary    = ["IN:Wireless Access Required",
-                                "IN:Wireless Access Patient Request",
-                                "IN:New User Access",
-                                "IN:Software Installation",
-                                "IN:Hardware Quotation"];
-    var cannedAnswersDetail     = ["Please can I make a request for wireless access for a monitor who is visiting the {unitName} unit on {AddDate}.\n\n" + 
-                                "They will be attending the unit for {noDays} day(s).The monitor's name is {monitorName}" + signOff,
-                                "Please can I make a request for wireless access for a patient visiting the {unitName} unit on {AddDate} and will require access for {n} day(s)."+signOff,
-                                "Please can you provide access for the following new user {name}. \n\nThey will require the same access as the {groupName}."+signOff,
-                                "I would like to make a request for a software Installation. The software I require installing is {SoftwarePackage}.\n\n"+
-                                "The package is to be installed on {My Current Computer Name} / {Another computer name} / {List of Computer Names}."+signOff,
-                                "Please will you provide a quotation for the purchase of IT equipment. The equipment required is as follows: \n\nUniversity Standard Desktop PC."+signOff];
-    var cannedAnswersCat1       = ["Access and Accounts",
-                                  "Access and Accounts",
-                                  "Filestore",
-                                  "Software",
-                                  "Hardware"];
-    var cannedAnswersCat2       = ["Guest Wireless",
-                                  "Guest Wireless",
-                                  "Shared Filestore",
-                                  "Software Installation",
-                                  "Desktop PC"];
+    var cannedAnswersSummary    = [];
+    var cannedAnswersDetail     = [];
+    

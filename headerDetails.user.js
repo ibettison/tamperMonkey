b0592c574d03af5098e1fc274dd005929b0dd4b9
ibettison <ian.bettison@ncl.ac.uk>
2020-07-08 07:35:09 +0100

    Changed some of the detail to reflect changes in location and telephone number

--- headerDetails.user.js
+++ headerDetails.user.js
@@ -1,7 +1,7 @@
     /*default values for new incident creation */
     var raisedUser              = "nib8";
     var raisedUserFull          = "Ian Bettison";
-    var location                = "Clinical Platforms";
+    var location                = "FMS NUIT";
     var impact                  = "Low";
     var urgency                 = "Low";
     var responseLevel           = "Priority 4 - Low";
@@ -9,8 +9,8 @@
     var ticketSource            = "Email";
     var linkToLanSweeper        = "http://crf-psrv:81/user.aspx?username=<<userName>>&userdomain=CAMPUS";
     /*variables for the signOff signature*/
-    var FMSLocation             = "Clinical Research Platforms";
-    var FMSTelephone            = "T: 0191 20 84650";
+    var FMSLocation             = "FMS NUIT";
+    var FMSTelephone            = "T: 0191 20 81377";
     var FMSEmail                = "E: ian.bettison@ncl.ac.uk";
     var signOff                 = "\n\nThanks for your attention,\n\n"+raisedUserFull+"\n"+FMSLocation+"\n"+FMSTelephone+"\n"+FMSEmail+"\n";
 
